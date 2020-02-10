defe8dc11b714b82d396df14ae66ed2ff1c5135b
jacksonvieira <jacksonsilvavieira@gmail.com>
2020-02-09 22:32:59 -0300

    changes os struct project and create a dashboard controller

--- src/main/java/com/authentication/SpringBootAuthJwtApplication.java
+++ src/main/java/com/authentication/SpringBootAuthJwtApplication.java
@@ -1,4 +1,4 @@
-package com.authentication.springbootauthjwt;
+package com.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
