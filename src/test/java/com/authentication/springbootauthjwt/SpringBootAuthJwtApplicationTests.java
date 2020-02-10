0da3b6ecabaf1e0be90fffde99a2f78d2a93b765
clodoaldo.silveira <clodoaldo.silveira@portotech.org>
2020-02-09 22:15:30 -0300

    setup

---
+++ src/test/java/com/authentication/springbootauthjwt/SpringBootAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.springbootauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
